7fbf3795eaab4f70199ed5f46702688fe4c37d83
Karan Singh <ZucM@users.noreply.github.com>
2021-08-16 12:02:44 +0530

    functionality done

---
+++ script.js
@@ -0,0 +1,34 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Your birthday is lucky ✨";
+    } else {
+        outputBox.innerText = "Your birthday is not lucky 😶";
+    }
+}
+
+function checkBirthdayIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if (sum && dob) {
+        compareValues(sum, luckyNumber.value);
+    } else {
+        outputBox.innerText = "Please enter both the fields 😑";
+    }
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener('click', checkBirthdayIsLucky);
